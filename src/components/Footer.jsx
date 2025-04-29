62efdf1137332a490d48b60768538968a765d6ad
Ace Malto <58543073+AceMalto@users.noreply.github.com>
2025-04-29 21:47:37 +0800

    modification on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,35 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className=''>
+            <div>
+                <input type="text" />
+                <button>
+                    Get Started 
+                </button>
+            </div>
 
+            <div>
+                <h3 className=' underline'>Questions? Contact us.</h3>
+                <div className=' grid grid-rows-5'>
+                    <p className=' underline'>FAQ</p>
+                    <p className=' underline'>Investor</p>
+                    <p className=' underline'>Ways to watch</p>
+                    <p className=' underline'>Corporate Information</p>
+                    <p className=' underline'>Only on Netflix</p>
+                    <p className=' underline'>Help Center</p>
+                    <p className=' underline'>Jobs</p>
+                    <p className=' underline'>Terms of Use</p>
+                    <p className=' underline'>Contact us</p>
+                    <p className=' underline'>Account</p>
+                    <p className=' underline'>Redeem Gift Cards</p>
+                    <p className=' underline'>Privacy</p>
+                    <p className=' underline'>Speed Test</p>
+                    <p className=' underline'>Media Center</p>
+                    <p className=' underline'>Buy Gift Cards</p>
+                    <p className=' underline'>Cookie Preferences</p>
+                    <p className=' underline'>Legal Notices</p>
+                </div>
+            </div>
         </div>
     )
 }
